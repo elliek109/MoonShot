9d6436aec0108d729ba6ebf0603c3c322f4e4349
ellie <elaina.kuhnke@stu.barren.kyschools.us>
2025-11-10 09:17:27 -0600

    Initial Commit

---
+++ MoonShot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  MoonShot
+//
+//  Created by Student on 11/7/25.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Student on 11/7/25.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
